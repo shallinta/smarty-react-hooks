8f91cb3e9b13abf686b9b487a721ee3e8f288a35
shallinta <740159135@qq.com>
2022-07-07 17:20:34 +0800

    fix: 增加 useUpdateEffect hooks 入口

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as useCountdown } from './hooks/use-countdown';
 export { default as useInput } from './hooks/use-input';
 export { default as useKeyDown } from './hooks/use-key-down';
 export { default as useDragNDrop } from './hooks/use-drag-n-drop';
+export { default as useUpdateEffect, useUpdateLayoutEffect, useFirstMountState } from './hooks/use-update-effect';
